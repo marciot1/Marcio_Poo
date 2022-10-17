6862df29411058a27591332e9bdb91c0d2e81a82
MARCIO TARGINO DO NASCIMENTO <nascimento.marcio@academico.ifrn.edu.br>
2022-10-17 15:54:44 -0300

    aula02

---
+++ Aula02/Aula02/contato.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Aula02
+{
+    class contato
+    {
+        private string nome, email, fone;
+        public contato(string n, string e, string f)
+        {
+            nome = n;
+            email = e;
+            fone = f;
+        }
+        public override string ToString()
+        {
+            return $"{nome} - {email} - {fone}";
+        }
+    }
+}
