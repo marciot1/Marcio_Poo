bf8c952a53e1fd8a06208f6264f8f198ee8b7fef
MARCIO TARGINO DO NASCIMENTO <nascimento.marcio@academico.ifrn.edu.br>
2022-10-24 15:55:38 -0300

    uipoad aula2

---
+++ Aula02/atividade/Abastecimento.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace atividade
+{
+    class Abastecimento
+    {
+        private double valorLitro;
+        private double ValorPago;
+        private double valorKmRodados;
+
+        public void SetValorLitro(double valorLitro)
+        {
+            if (valorLitro > 0) this.valorLitro = valorLitro;
+        }
+        public void SetvalorPago(double valorPago)
+        {
+            if (valorPago > 0) this.valorPago = valorPago;
+        }
+        public void SetKmRodados(double KmRodado)
+        {
+            if (KmRodado > 0) this.KmRodados = KmRodados;
+        }
+        public double MediaKmLitro()
+        {
+            double numLitros = valorLitro;
+            return KmRodados / numLitros;
+        }
+        public double MediaReaisKm()
+        {
+            return ValorPago / KmRodados();
+        }
+    }
+}
